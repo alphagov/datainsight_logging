4733688e5a7eaab520110793cafdde8ede334616
Johannes Thoenes <jthoenes@thoughtworks.com>
2012-08-28 16:48:11 +0100

    Add support for multiple log files per project.
    author: @jthoenes
    author: @pbadenski

--- lib/datainsight_logging.rb
+++ lib/datainsight_logging.rb
@@ -5,21 +5,23 @@ module Datainsight
 
   module Logging
 
-    def self.configure(env=ENV["RACK_ENV"])
+    def self.configure(params={})
+      env = params[:env] || ENV["RACK_ENV"]
+      type = params[:type]
       case (env or "default").to_sym
         when :test
           configure_test
         when :production
-          configure_production
+          configure_production(type)
         else
-          configure_development
+          configure_development(type)
       end
     end
 
     private
-    def self.configure_development
+    def self.configure_development type
       ::Logging.appenders.stdout(:stdout)
-      ::Logging.appenders.rolling_file(:file, :filename => 'log/development.log',
+      ::Logging.appenders.rolling_file(:file, :filename => log_file_location(type, :development),
                                      :age => 'daily')
       ::Logging.appenders.growl(:growl, :level => :error,
                               :layout => ::Logging::Layouts.pattern(:pattern => '%-5l: %m\n'))
@@ -28,8 +30,8 @@ module Datainsight
       ::Logging.logger.root.level = :debug
     end
 
-    def self.configure_production
-      ::Logging.appenders.rolling_file(:file, :filename => 'log/production.log',
+    def self.configure_production type
+      ::Logging.appenders.rolling_file(:file, :filename => log_file_location(type, :production),
                                      :age => 'daily', :layout => ::Logging::Layouts.json)
 
       ::Logging.logger.root.appenders = [:file]
@@ -41,6 +43,10 @@ module Datainsight
 
       ::Logging.logger.root.appenders = [:stdout]
       ::Logging.logger.root.level = :info
+    end
+
+    def self.log_file_location type, env
+      type ? "log/#{type}-#{env}.log" : "log/#{env}.log"
     end
 
     module Helpers
